2fa9f33e960c07f7ee320111b8fd6da69d3081f9
ItsukiMurayama <s1711472@s.tsukuba.ac.jp>
2019-08-05 14:13:06 +0900

    ゲーム追加画面

---
+++ shlack/shlack/AddViewController.swift
@@ -0,0 +1,16 @@
+//
+//  AddViewController.swift
+//  shlack
+//
+//  Created by Itsuki Murayama on 2019/08/05.
+//  Copyright © 2019 畑田将太. All rights reserved.
+//
+import UIKit
+import RealmSwift
+
+class AddViewController: UIViewController {
+    @IBOutlet weak var all: UIButton!
+    
+    override func viewDidLoad() {
+    }
+}
